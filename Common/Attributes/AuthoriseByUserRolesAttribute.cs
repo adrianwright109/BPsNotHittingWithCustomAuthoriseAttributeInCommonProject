4f1fadc256d36f58de4f20d19cc074c8604630ad
Adrian Wright <awright@keeresources.com>
2021-03-22 19:00:08 +0000

    Add project files.

---
+++ Common/Attributes/AuthoriseByUserRolesAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Common.Enums;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Common.Attributes
+{
+    public class AuthoriseByUserRolesAttribute : AuthorizeAttribute
+    {
+        public AuthoriseByUserRolesAttribute(params UserRole[] userRoles)
+        {
+            if (userRoles.Length > 0)
+            {
+                Roles = string.Join(',', userRoles.Select(x => x.ToString()));
+            }
+        }
+    }
+}
---
+++ Common/Enums/UserRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Common.Enums
+{
+    [Serializable]
+    public enum UserRole
+    {
+        [Description("Admin User")]
+        AdminUser = 'A',
+
+        [Description("Standard User")]
+        StandardUser = 'U',
+
+        [Description("Read-Only User")]
+        ReadOnlyUser = 'R'
+    }
+}
